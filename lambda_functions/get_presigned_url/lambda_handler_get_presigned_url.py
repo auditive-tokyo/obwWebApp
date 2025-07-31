7b8a4f4329eca9fe4b3228a979b0bf8a5672b782
Auditive <info@auditive.tokyo>
2025-08-01 02:00:38 +0900

    コード内でCORS設定してた、、、。

--- lambda_functions/get_presigned_url/lambda_handler_get_presigned_url.py
+++ lambda_functions/get_presigned_url/lambda_handler_get_presigned_url.py
@@ -34,6 +34,5 @@ def lambda_handler(event, context):
 
     return {
         "statusCode": 200,
-        "headers": {"Access-Control-Allow-Origin": "*"},
         "body": json.dumps({"url": url})
     }
